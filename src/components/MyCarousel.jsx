efe7df35d6f856d8530e5353f678a80ba2ebf4a4
Kkilker <kkilker@vigo.lib.in.us>
2023-05-18 17:00:18 -0400

    carousel and layout

--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -4,23 +4,22 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 const carouselStyle = {
-    height: '100vh',  
-    width: '80vw',
+    height: '100%',  
+    width: '100%',
     backgroundSize: 'cover',
     objectFit: "cover",
-    zIndex: 1,
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.9)', 
     borderRadius: '10px',
     padding: 0
 }
 
 const carouselDiv = {
-    height: '100vh',
-    width: '80vw'
+  height: '100%',
+  width: '100%',
 }
 
 const slideContentStyle = {
-    position: 'absolute',
+  position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
@@ -29,6 +28,7 @@ const slideContentStyle = {
     color: 'white',
     marginBottom: '20px',
     textShadow: '1px 2px solid black'
+
   };
   
 
@@ -40,6 +40,10 @@ const imageStyle = {
     opacity: '50%'
 }
 
+const titleStyle = {
+  fontSize: '5rem'
+}
+
 const MyCarousel = () => {
   return (
     <div style={carouselStyle}>
@@ -49,16 +53,21 @@ const MyCarousel = () => {
         showThumbs={false}
         showArrows={false}>
         <div style={carouselDiv}>
-          <img src="pexels-ray-bilcliff-1494707.jpg" style={imageStyle} />
-          <div style={slideContentStyle}><h1><i>Song of the Sorcerer</i></h1>
-          <p><span class="first-letter">I</span>n a land once consumed by power struggles over the Oracle and the Draftsman, a man hiding as an apothecary defies his birthright to protect his mentor, only to have his hidden abilities exposed when their enclave is raided by the oppressive Mortal Order -- and almost used to destroy the world for the second time.</p>
-          <p>On the run from a cruel and unusual regime, Andrey and the unwilling oracle Elitza must master their powers and navigate complex alliances to defeat the oracle Marija before the cataclysmic spell bound to Andrey's flesh can manifest.</p>
-          <p>"Song of the Sorcerer" delves into themes of love, resilience, and the enduring impact of trauma in a world teetering on the brink of doom, where hope remains steadfast.</p>
+          <img src="DallE SotS.png" style={imageStyle} />
+          <div style={slideContentStyle}><h1 style={titleStyle}><i>Song of the Sorcerer</i></h1>
+          <p><span class="first-letter">I</span>n a land once consumed by power struggles over the Oracle and the Draftsman, healer Andrey is captured by the Mortal Order and becomes a weapon of destruction. </p>
+          <p>Now on the run, he must defy the cataclysmic spell bound to his flesh before it can take hold.</p>
+          <p>"Song of the Sorcerer" delves into themes of love, resilience, the enduring legacy of trauma, and finding hope in a doomed world.</p>
           <p> With its intricate magic, palpable political tension, complex characters, and immersive world-building, this gripping tale is sure to captivate readers.</p>
 </div>
         </div>
         <div style={carouselDiv}>
-          <img src="pexels-lucas-pezeta-3772353.jpg" style={imageStyle}/>
+          <img src="DallE Bidding.png" style={imageStyle}/>
+          <div style={slideContentStyle}><h1 style={titleStyle}><i>Bidding</i></h1>
+        <p><span class="first-letter">L</span>ife is peaceful in Latra; as long as one believes and obeys.</p><p>
+          <p>In this dark YA fantasy inspired by the paintings of Heironymous Bosch, nothing is as it seems and one must be careful who--or what--they worship.</p>
+        </p>
+        </div>
         </div>
         <div style={carouselDiv}>
           <img src="pexels-pixabay-277552.jpg" style={imageStyle} />
